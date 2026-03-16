6575c76e0e0f0d6531d24ba0e42b6633d679c63f
marcoyimyaem <marcoyimyaem@gmail.com>
2026-03-16 04:07:25 +0000

    Day 7

---
+++ Test/UserTest.java
@@ -0,0 +1,8 @@
+package Test;
+import chapter1.Toy;
+
+public class UserTest {
+    public static void main(String[] args) {
+        Toy toy1 = new Toy();
+    }
+}
---
+++ chapter1/Main.java
@@ -0,0 +1,14 @@
+package chapter1;
+// sample file
+public class Main{
+    static public void main(String[] args){
+               
+/*
+ * this
+ * is
+ * a 
+ * multi-line comment
+ */
+        
+    }
+}
