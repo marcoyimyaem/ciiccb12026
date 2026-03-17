2c30c5e4e6a1359fc550fb9dadd0c16d3e8a7795
marcoyimyaem <marcoyimyaem@gmail.com>
2026-03-17 04:04:33 +0000

    Day 8

--- chapter1/Main.java
+++ chapter1/Main.java
@@ -1,14 +1,28 @@
-package chapter1;
 // sample file
+package chapter1;
+
+import java.util.*;
+
+
+
 public class Main{
-    static public void main(String[] args){
-               
-/*
- * this
- * is
- * a 
- * multi-line comment
- */
-        
+    public static void main(String[] args) {
+        //create and instance of a Toy class = Toy object
+
+        /*   this
+        *    is a
+        * multiline 
+        *comment
+*/
+        Toy toy1 = new Toy();
+        toy1.name ="Rage pink";
+        toy1.brand = "Lab Vuvu";
+        toy1.price = 4500;
+        toy1.quantity = 12;
+        System.out.println();
+        Random r = new Random();
+        System.out.println(r.nextInt(100));
+        ArrayList al = new ArrayList<>();
+        Date date1 = new Date();
     }
 }
