f81f4373f7c5ffe5e52c602d91324640d635a5d7
marcoyimyaem <marcoyimyaem@gmail.com>
2026-03-21 08:03:31 +0000

    Day 10

--- Test/UserTest.java
+++ Test/UserTest.java
@@ -1,8 +1,9 @@
-package Test;
-import chapter1.Toy;
+
+// import chapter1.Toy;
 
 public class UserTest {
     public static void main(String[] args) {
-        Toy toy1 = new Toy();
+        // Toy toy1 = new Toy();
+        System.out.println(args[1]);
     }
 }
