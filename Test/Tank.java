c43018883b93ccc3f0d7408a7dd68de4e94cdf37
marcoyimyaem <marcoyimyaem@gmail.com>
2026-03-23 04:09:45 +0000

    Day 12

---
+++ Test/Tank.java
@@ -0,0 +1,10 @@
+package Test;
+public class Tank {
+    public void print(Water water){
+        System.out.println(water);
+    }
+    public static void main(String[] args) {
+        
+    }
+
+}
