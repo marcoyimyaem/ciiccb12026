c276d092f0dc00c8b10d5e6dfe34566475b69324
marcoyimyaem <marcoyimyaem@gmail.com>
2026-04-11 12:59:50 +0000

    Day 25

--- Test/UserTest.java
+++ Test/UserTest.java
@@ -1,9 +1,19 @@
 package Test;
 // import chapter1.Toy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserTest {
+
     public static void main(String[] args) {
         // Toy toy1 = new Toy();
-        System.out.println(args[1]);
+        // System.out.println(args[1]);
+        Map<String,Integer> prices = new HashMap<>();
+        System.out.println(prices.put("LPG", 1600));
+        System.out.println(prices.put("Gas", 98));
+        System.out.println(prices.put("LPG", 1600));
+        System.out.println(prices.get("Gas"));
     }
+    
 }
