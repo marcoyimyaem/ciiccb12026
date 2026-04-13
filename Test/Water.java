12e91b9c34dec1f2a98b6696bcf489aafd5c2dca
marcoyimyaem <marcoyimyaem@gmail.com>
2026-04-13 03:58:56 +0000

    Day 26

--- Test/Water.java
+++ Test/Water.java
@@ -1,5 +1,14 @@
 package Test;
 
 public class Water {
+    public static void main(String[] args) {
+        var title = new ThatTimeIWasReincarnatedAsAFilipinoProgrammerThatIEnjoySoMuchITriedJavaForFunButEndupInMisseryThatICreatedMyOwnReligion();
+    }
+}
+
+
+
+
+class ThatTimeIWasReincarnatedAsAFilipinoProgrammerThatIEnjoySoMuchITriedJavaForFunButEndupInMisseryThatICreatedMyOwnReligion{
 
 }
